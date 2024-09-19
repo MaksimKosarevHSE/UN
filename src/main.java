eceafcf5a28bcc5bd2d435a474a0e2fc70b5c886
Max <maksimkosarev96@gmail.com>
2024-09-19 23:12:04 +0300

    SUKA

--- src/main.java
+++ src/main.java
@@ -2,7 +2,8 @@ public class main {
     public static void main(String[] args) {
         System.out.println("hello world!!");
         System.out.println("Hello world2");
-        int a = 10;
+        int a = 1;
+        int b = 20;
         a++;
     }
 }
